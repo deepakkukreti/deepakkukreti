32795f69837c0602e19e1d01d22a4429df8511bb
deepakkukreti <91362089+deepakkukreti@users.noreply.github.com>
2021-10-09 12:11:42 +0530

    Add files via upload

---
+++ ascii.cpp
@@ -0,0 +1,13 @@
+//program for ASCII//
+//DEEPAK CHAND KUKRETI//
+//M_20011568//
+#include <iostream>
+using namespace std;
+int main()
+{
+char ch;
+cout<<"Enter the Character\n";
+cin>>ch;
+int a=ch;
+cout<<"ascii value is\n"<<a;
+}
