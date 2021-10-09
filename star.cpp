3869390706419ee79fcc9ec7cc6ecd792fe332ff
deepakkukreti <91362089+deepakkukreti@users.noreply.github.com>
2021-10-09 12:47:36 +0530

    Add files via upload

---
+++ star.cpp
@@ -0,0 +1,25 @@
+//DEEPAK CHAND KUKRETI//
+//M//
+//20011568//
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+   int rows;
+   
+   cin >> rows;
+   
+   for(int i=0 ;i <= rows ; ++i)
+   {
+      for(int j=1;j<=i;++j)
+      {
+         cout << "*";
+      }
+
+   cout << "" << endl;
+   }
+}           
